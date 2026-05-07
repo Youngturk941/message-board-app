795e2f766e0d725976482425593052c014ccecf8
Olanipekun Boluwatife Valentine <olanipekunboluwatife50@gmail.com>
2026-05-07 01:04:55 +0100

    fix contract artifact path for deployment

--- src/messageBoardContract.js
+++ src/messageBoardContract.js
@@ -1,4 +1,4 @@
-import artifact from '../../message-board-contracts/artifacts/contracts/MessageBoard.sol/MessageBoard.json';
+import artifact from './MessageBoard.json';
 
 export const messageBoardAddress = '0xcB5Fc91f097DF9DCC3260dEf205afF083799cF7E';
 export const messageBoardAbi = artifact.abi;
